54abb3835792af1456044cd4cf6f634d8467dcae
Aavash-kc-01 <aavashk71@gmail.com>
2021-12-10 02:20:01 +0545

    completed coding challenge

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_TEXT =
+            "com.example.twoactivities.extra.TEXT";
+    private String string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendTextOne(View view) {
+        string = getString(R.string.article_text_one);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_TEXT, string);
+        startActivity(intent);
+    }
+
+    public void sendTextTwo(View view) {
+        string = getString(R.string.article_text_two);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_TEXT, string);
+        startActivity(intent);
+    }
+
+    public void sendTextThree(View view) {
+        string = getString(R.string.article_text_three);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_TEXT, string);
+        startActivity(intent);
     }
 }
